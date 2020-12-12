e5c930b5b3781bcd0dcb1c41920aa1f8fca91270
enmust <enmust@users.noreply.github.com>
2020-12-12 15:32:49 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? process.env.PRODUCTION_GITHUB ? "/resourcing" : "/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/resourcing" : "/" : "/"
 };
